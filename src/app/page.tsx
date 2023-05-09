e083104794fad5581ac42d7bdf156d860bfb1094
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-09 18:48:37 +0530

    Tweak Coming Soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import RootLayout from "./layout";
+import styles from "./Home.module.css"
 
 export default function Home() {
   return (
-    <div>Coming Soon!</div>
+    <div className={styles.content}>Coming Soon!</div>
   );
 }
