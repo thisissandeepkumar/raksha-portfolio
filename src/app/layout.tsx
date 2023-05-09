59513edea205ab688035c3d4efef62a8ea8f436a
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-09 18:49:44 +0530

    Changed URLs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,48 +16,39 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <body>
         <nav className="main-nav">
           <ul className="nav-ul">
-            <Link
-              className="nav-li"
-              href="https://github.com/thisissandeepkumar/raksha-portfolio"
-            >
+            <Link className="nav-li" href="#">
               MY PROJECTS
             </Link>
-            <Link
-              className="nav-li"
-              href="https://github.com/thisissandeepkumar/raksha-portfolio"
-            >
+            <Link className="nav-li" href="#">
               ABOUT ME
             </Link>
-            <Link
-              className="nav-li"
-              href="https://github.com/thisissandeepkumar/raksha-portfolio"
-            >
+            <Link className="nav-li" href="#">
               MY BLOGS
             </Link>
-            <Link
-              className="nav-li"
-              href="https://github.com/thisissandeepkumar/raksha-portfolio"
-            >
+            <Link className="nav-li" href="#">
               CONTACT ME
             </Link>
-            <Link
-              className="nav-li"
-              href="https://github.com/thisissandeepkumar/raksha-portfolio"
-            >
+            <Link className="nav-li" href="#">
               SUPPORT
             </Link>
           </ul>
-          <Link href="https://google.co.in">
+          <Link href="#">
             <button className="dark-button">RESUME ↗</button>
           </Link>
         </nav>
