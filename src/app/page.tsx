90768a58ebf58278773c8031ea3ff39894cfa7fa
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-09 22:43:06 +0530

    Basic Welcome Card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
-import RootLayout from "./layout";
-import styles from "./Home.module.css"
+import Image from "next/image";
+import "./home.css"
 
 export default function Home() {
   return (
-    <div className={styles.content}>Coming Soon!</div>
+    <div>
+      <WelcomeCard />
+    </div>
+  );
+}
+
+function WelcomeCard() {
+  return (
+    <div className="welcome-card">
+      <div className="card-content">
+        <div className="welcome-card-text-content">
+          <p className="welcome-1">Hey, I'm Raksha 👋</p>
+          <p className="welcome-2">Welcome to My UI & UX Design Portfolio</p>
+        </div>
+        <div>
+          <Image
+            src="/images/raksha.png"
+            alt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV3qnIImwUBUIdIH8nYCtycbzK3vwaFnKKIOQQaa7R&s"
+            width={473}
+            height={560}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
