bfd209516f10fd9a9dfc60e665d9b1cde2f0729c
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-09 14:44:03 +0530

    Global Layout added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,11 @@
-import Head from "next/head";
-import Script from "next/script";
+import RootLayout from "./layout";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-          crossOrigin="anonymous"
-        />
-      </Head>
-
-      <Script>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-          crossOrigin="anonymous"
-        />
-      </Script>
-      <div>Test Page</div>
+      <RootLayout>
+        <div>Test Page</div>
+      </RootLayout>
     </>
   );
 }
