0026fac4703dfb12d501bb9ca27f1c930f2ca404
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-10 13:36:39 +0530

    Hide li on small screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,20 +18,20 @@ export default function Home() {
 
 function WelcomeCard() {
   return (
-    <div className="welcome-card">
-      <div className="card-content">
-        <div className="welcome-card-text-content">
-          <p className="welcome-1">Hey, I'm Raksha 👋</p>
-          <p className={`${broadsheetLdoFont.variable} welcome-2`}>Welcome to My UI & UX Design Portfolio</p>
-        </div>
-        <div>
-          <Image
-            src="/images/raksha.png"
-            alt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV3qnIImwUBUIdIH8nYCtycbzK3vwaFnKKIOQQaa7R&s"
-            width={473}
-            height={560}
-          />
-        </div>
+    <div className="card-content">
+      <div className="welcome-card-text-content">
+        <p className="welcome-1">Hey, I'm Raksha 👋</p>
+        <p className={`${broadsheetLdoFont.variable} welcome-2`}>
+          Welcome to My UI & UX Design Portfolio
+        </p>
+      </div>
+      <div>
+        <Image
+          src="/images/raksha.png"
+          alt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV3qnIImwUBUIdIH8nYCtycbzK3vwaFnKKIOQQaa7R&s"
+          width={473}
+          height={560}
+        />
       </div>
     </div>
   );
