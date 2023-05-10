3235198c5b9123f5d64bd98e7078ca3417176da0
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-10 18:28:24 +0530

    Added About Me Content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,6 +125,31 @@ function AboutMe() {
         About {<ColouredText text="Me" backgroundColor="#E5E2F6" />}
       </h3>
       <hr className="solid-divider" />
+      <div className="about-me-content">
+        <div className="about-me-pic">
+          <Image src="/images/raksha-small.png" alt="Raksha" fill />
+        </div>
+        <div className="about-me-description">
+          <p>
+            I'm a 22 y/o CSE Graduate with strong passion for UI & UX Designing.
+            I’ve been designing for 2+ Years and currently work as a
+            Professional UX designer.
+          </p>
+          <p>
+            I believe that user experience design is a wonderful blend of
+            innovation and human psychology, and through my work, I have come to
+            understand the power of digital products paired with compassion and
+            empathy towards users. So far I have designed for domains like
+            Web3.0, Fintech, Agriculture, LMS, Logistics, AI-based Systems and
+            CRMs.
+          </p>
+          <p>
+            When I'm not designing, you can find me sketching zen art and
+            painting. I'm also a huge fan of dogs, documentaries, and design -
+            not necessarily in that order!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
