61f6c348af2172c6c48109b5061f0204ae813ba3
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-05-15 14:59:56 +0530

    Caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,12 @@ type Data = [
 
 async function getRSSData() {
   const res = await fetch(
-    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@rakshakaranth.s"
+    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@rakshakaranth.s",
+    {
+      next: {
+        revalidate: 3600,
+      }
+    }
   );
   if(!res.ok) {
     throw new Error("Something went wrong");
